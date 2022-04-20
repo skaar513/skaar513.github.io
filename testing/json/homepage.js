a01b98c27880b1a8f2e5ede39eaeab8dc9f583c9
skaar513 <skaar513@gmail.com>
2022-04-20 14:45:58 +0530

    testing out layout for bookmarks

--- testing/json/homepage.js
+++ testing/json/homepage.js
@@ -1,6 +1,6 @@
 const allEngines = [
  { name:"Brave", color:"#FC8A84", url:"search.brave.com/search?q=" },
  { name:"SearX", color:"#8AE29D", url:"searx.be/search?q=" },
- { name:"ArchWiki", color:"#A8BEFB", url:"wiki.archlinux.org/index.php?search=" },
- { name:"YouTube", color:"#FBE091", url:"youtube.com/results?search_query=" }
+ { name:"YouTube", color:"#FBE091", url:"youtube.com/results?search_query=" },
+ { name:"ArchWiki", color:"#A8BEFB", url:"wiki.archlinux.org/index.php?search=" }
 ];
