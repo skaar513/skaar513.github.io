968e280c40251ba0f44dda03dd2c2a65f7caae61
skaar513 <skaar513@gmail.com>
2022-04-21 20:43:52 +0530

    removed unnecessary comments

--- testing/js/allEngines.js
+++ testing/js/allEngines.js
@@ -1,7 +1,5 @@
 const allEngines = [
- { name:"Brave", color:"#FC8A84", url:"search.brave.com/search?q=" },
-// { name:"SearX", color:"#8AE29D", url:"searx.be/search?q=" },
- { name:"Wikipedia", color:"#FBE091", url:"wikipedia.org/w/index.php?search=" },
- { name:"YouTube", color:"#FBE091", url:"youtube.com/results?search_query=" },
- { name:"ArchWiki", color:"#A8BEFB", url:"wiki.archlinux.org/index.php?search=" }
+ { name:"Brave", preposition:"with", color:"#FC8A84", url:"search.brave.com/search?q=", urlFilters:"" },
+ { name:"Wikipedia", preposition:"over", color:"#FBE091", url:"wikipedia.org/w/index.php?search=", urlFilters:"" },
+ { name:"Github", preposition:"over", color:"#FBE091", url:"github.com/search?q=", urlFilters:"&s=stars" }
 ];
--- testing/js/functions.js
+++ testing/js/functions.js
@@ -1,19 +1,18 @@
-	var activeEngine, url;
+	var activeEngine;
 	var x = -1;
 
 	function toggleEngine() { x = (x == allEngines.length - 1) ? -1:x; 
 		x = x + 1;
 		activeEngine = allEngines[x];
-//		document.getElementById('toggleButton').style.color = activeEngine.color;
-		document.getElementById('searchInput').placeholder = "Search with " + activeEngine.name;
-		url = activeEngine.url; 
+		document.getElementById('searchInput').placeholder = "Search " + activeEngine.preposition + " " + activeEngine.name;
 		document.getElementById('searchInput').focus(); };
 
-	function search() {let realVal = document.getElementById('searchInput').value.replace("%","%25").replace(/#/g,"%23").replace(/&/g,"%26").replaceAll("+","%2B").replace("/","%2F").replace("?","%3F").replace("=","%3d").replace("_","%5F").replace(/:/g,"%3A");
-			let noSpaceVal = realVal.replace(/\s/g, "");
-			{if (noSpaceVal == "") {window.location.reload(false);} else {window.open("https://" + url + realVal, "_self");}
-				}
-				  };
+	function search() { let realVal = document.getElementById('searchInput').value.replace("%","%25").replace(/#/g,"%23").replace(/&/g,"%26").replaceAll("+","%2B").replace("/","%2F").replace("?","%3F").replace("=","%3d").replace("_","%5F").replace(/:/g,"%3A");
+			    let noSpaceVal = realVal.replace(/\s/g, "");
+				{ if (noSpaceVal == "") {window.location.reload(false);}
+				  else {window.open("https://" + activeEngine.url + realVal + activeEngine.urlFilters, "_self");} };
+	};
+
 	function clearInput() { document.getElementById("searchInput").value = "";
 				document.getElementById('searchInput').focus();
 				hideClearButton(); };
@@ -24,6 +23,3 @@
 	function hideClearButton() {
 			document.getElementById("clearButton").style.display = (document.getElementById("searchInput").value == "") ? "none":"inline";
 		};
-
-//	function backToStart() { let editContainer = document.getElementById("editContainer");
-//				   editContainer.style. ;};
