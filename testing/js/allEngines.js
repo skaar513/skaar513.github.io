2345107ea00fbae37a74c65e89722f9c58ca2f7b
skaar513 <skaar513@gmail.com>
2022-04-21 20:10:20 +0530

    started building the edit menu and moved scripts to .js files

--- testing/js/allEngines.js
+++ testing/js/allEngines.js
@@ -1,6 +1,7 @@
 const allEngines = [
  { name:"Brave", color:"#FC8A84", url:"search.brave.com/search?q=" },
- { name:"SearX", color:"#8AE29D", url:"searx.be/search?q=" },
+// { name:"SearX", color:"#8AE29D", url:"searx.be/search?q=" },
+ { name:"Wikipedia", color:"#FBE091", url:"wikipedia.org/w/index.php?search=" },
  { name:"YouTube", color:"#FBE091", url:"youtube.com/results?search_query=" },
  { name:"ArchWiki", color:"#A8BEFB", url:"wiki.archlinux.org/index.php?search=" }
 ];
