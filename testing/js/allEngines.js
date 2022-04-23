777764f47c024841fdcf9a3099ca2a0bc8615414
skaar513 <skaar513@gmail.com>
2022-04-23 18:23:06 +0530

    improved the "edit" menu

--- testing/js/allEngines.js
+++ testing/js/allEngines.js
@@ -1,5 +1,5 @@
 const allEngines = [
  { name:"Brave", preposition:"with", color:"#FC8A84", url:"search.brave.com/search?q=", urlFilters:"" },
- { name:"Wikipedia", preposition:"over", color:"#FBE091", url:"wikipedia.org/w/index.php?search=", urlFilters:"" },
+ { name:"Wikipedia", preposition:"over", color:"pink", url:"wikipedia.org/w/index.php?search=", urlFilters:"" },
  { name:"Github", preposition:"over", color:"#FBE091", url:"github.com/search?q=", urlFilters:"&s=stars" }
 ];
--- testing/js/editMenu.js
+++ testing/js/editMenu.js
@@ -1,10 +1,22 @@
 toggleEngine();
 let testLink = document.getElementById("link1");
-document.getElementById("desc").innerHTML = testLink.href + " - " + testLink.innerHTML;
-document.getElementById("desc2").innerHTML = allEngines[0].name + "\n" + allEngines[1].name + allEngines[2].name;
+document.getElementById("engine0Name").innerHTML = allEngines[0].name
+document.getElementById("engine0Name").style.color = allEngines[0].color
+document.getElementById("engine0Preposition").innerHTML = "Search \"" + allEngines[0].preposition + "\""
+document.getElementById("engine0Url").innerHTML = "https://" + allEngines[0].url + "{...}" + allEngines[0].urlFilters
+
+document.getElementById("engine1Name").innerHTML = allEngines[1].name
+document.getElementById("engine1Name").style.color = allEngines[1].color
+document.getElementById("engine1Preposition").innerHTML = "Search \"" + allEngines[1].preposition + "\""
+document.getElementById("engine1Url").innerHTML = "https://" + allEngines[1].url + "{...}" + allEngines[1].urlFilters
+
+document.getElementById("engine2Name").innerHTML = allEngines[2].name
+document.getElementById("engine2Name").style.color = allEngines[2].color
+document.getElementById("engine2Preposition").innerHTML = "Search \"" + allEngines[2].preposition + "\""
+document.getElementById("engine2Url").innerHTML = "https://" + allEngines[2].url + "{...}" + allEngines[2].urlFilters
 
 function editBookmarks() {
-	document.getElementById("editContainer").style.display = "inline";
+	document.getElementById("editContainer").style.display = "initial";
 	document.getElementById("mainContainer").style.opacity = "0.25";
 	document.getElementById("mainContainer").style["pointer-events"] = "none";
 }
@@ -16,7 +28,7 @@ function backToHome() {
 }
 
 function editSettings() {
-	document.getElementById("editContainer2").style.display = "inline";
+	document.getElementById("editContainer2").style.display = "initial";
 	document.getElementById("mainContainer").style.opacity = "0.25";
 	document.getElementById("mainContainer").style["pointer-events"] = "none";
 }
